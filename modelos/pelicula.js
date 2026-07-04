28a214528ee8f3e556bca6a114c1bb30ca9ea727
Nicolas <ngramirez313an@gmail.com>
2026-07-03 21:18:47 -0300

    feat: Clase Hija Configurada

--- modelos/pelicula.js
+++ modelos/pelicula.js
@@ -1,3 +1,82 @@
 {
   /** Clase hija Pelicula */
 }
+
+import { Produccion } from "./produccion.js";
+
+export class Pelicula extends Produccion {
+  #director;
+  #reparto;
+  #genero;
+  #clasificacion;
+  #clasificacionEdad;
+
+  constructor(data) {
+    super(data.id, data.title, data.overview, data.poster_path);
+    this.#director =
+      // ternaria
+      data.credits.crew.find((miembro) => miembro.job === "Director")?.name ||
+      "Director desconocido";
+    // Calificación
+    this.#clasificacion = data.vote_average.toFixed(1);
+    this.#reparto = data.credits.cast
+      .slice(0, 5)
+      .map((actor) => actor.name)
+      .join(", ");
+    this.#genero = data.genres
+      ? data.genres.map((genres) => genres.name).join(", ")
+      : "Sin Género";
+
+    if (data.credits) {
+      const directorData = data.credits.crew.find(
+        (miembro) => miembro.job === "Director",
+      );
+
+      this.#director = directorData
+        ? directorData.name
+        : "Director desconocido";
+      this.#reparto = data.credits.cast
+        .slice(0, 5)
+        .map((actor) => actor.name)
+        .join(", ");
+    } else {
+      this.#director = "Director desconocido";
+      this.#reparto = "Reparto desconocido";
+    }
+
+    if (data.release_dates) {
+      const cetAR = data.release_dates.results.find(
+        (pais) => pais.iso_3166_1 === "AR",
+      );
+      this.#clasificacionEdad =
+        cetAR && cetAR.release_dates[0].certification !== ""
+          ? certAR.release_dates[0].certification
+          : "S/C";
+    } else {
+      this.#clasificacionEdad = "S/C";
+    }
+  }
+
+  get director() {
+    return this.#director;
+  }
+  get reparto() {
+    return this.#reparto;
+  }
+
+  get genero() {
+    return this.#genero;
+  }
+
+  get clasificacion() {
+    return this.#clasificacion;
+  }
+
+  get clasificacionEdad() {
+    return this.#clasificacionEdad;
+  }
+
+  obtenerTipo() {
+    return "Pelicula";
+  }
+}
